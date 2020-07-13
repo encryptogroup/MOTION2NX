adf7f8d73261d10798afed27ea1d43af3a18ae48
Lennart Braun <lenerd@posteo.de>
2020-07-13 16:15:03 +0200

    add maxpool circuit

--- src/motioncore/algorithm/circuit_loader.cpp
+++ src/motioncore/algorithm/circuit_loader.cpp
@@ -210,4 +210,114 @@ const ENCRYPTO::AlgorithmDescription& CircuitLoader::load_gtmux_circuit(std::siz
   return algo_cache_[name];
 }
 
+const ENCRYPTO::AlgorithmDescription& CircuitLoader::load_tree_circuit(const std::string& algo_name,
+                                                                       std::size_t bit_size,
+                                                                       std::size_t num_inputs) {
+  if (num_inputs < 2) {
+    throw std::logic_error("need at least two inputs to combine");
+  }
+
+  const auto name = fmt::format("__circuit_loader_builtin__tree__{}__{}_bit", algo_name, bit_size);
+  auto it = algo_cache_.find(name);
+  if (it != std::end(algo_cache_)) {
+    return it->second;
+  }
+
+  const auto& sub_algo = algo_cache_.at(algo_name);
+  if (sub_algo.n_input_wires_parent_a_ != bit_size ||
+      !sub_algo.n_input_wires_parent_b_.has_value() ||
+      *sub_algo.n_input_wires_parent_b_ != bit_size || sub_algo.n_output_wires_ != bit_size) {
+    throw std::logic_error("invalid algorithm for tree building");
+  }
+
+  ENCRYPTO::AlgorithmDescription tree_algo{
+      .n_output_wires_ = bit_size,
+      .n_input_wires_parent_a_ = bit_size * num_inputs,
+      .n_wires_ = bit_size + (num_inputs - 1) * (sub_algo.n_wires_ - bit_size),
+      .n_gates_ = (num_inputs - 1) * sub_algo.n_gates_,
+      .n_input_wires_parent_b_ = std::nullopt,
+      .gates_ = {}};
+
+  // store the wire offsets of the current values
+  std::queue<std::size_t> values;
+  for (std::size_t i = 0; i < num_inputs; ++i) {
+    values.push(i * bit_size);
+  }
+  std::size_t wire_offset = bit_size * num_inputs;  // number of input wires
+  // std::size_t gate_offset = 0;
+  while (values.size() > 1) {
+    auto input_a_offset = values.front();
+    values.pop();
+    auto input_b_offset = values.front();
+    values.pop();
+
+    std::transform(
+        std::begin(sub_algo.gates_), std::end(sub_algo.gates_),
+        std::back_inserter(tree_algo.gates_),
+        [bit_size, wire_offset, input_a_offset, input_b_offset](ENCRYPTO::PrimitiveOperation op) {
+          if (op.parent_a_ < bit_size) {
+            op.parent_a_ += input_a_offset;
+          } else if (op.parent_a_ < 2 * bit_size) {
+            op.parent_a_ = op.parent_a_ - bit_size + input_b_offset;
+          } else {
+            op.parent_a_ = op.parent_a_ - 2 * bit_size + wire_offset;
+          }
+          if (op.parent_b_.has_value()) {
+            if (op.parent_b_ < bit_size) {
+              *op.parent_b_ += input_a_offset;
+            } else if (op.parent_b_ < 2 * bit_size) {
+              *op.parent_b_ = *op.parent_b_ - bit_size + input_b_offset;
+            } else {
+              *op.parent_b_ = *op.parent_b_ - 2 * bit_size + wire_offset;
+            }
+          }
+          op.output_wire_ = op.output_wire_ - 2 * bit_size + wire_offset;
+          return op;
+        });
+    wire_offset += sub_algo.n_wires_ - 2 * bit_size;
+    values.push(wire_offset - bit_size);
+  }
+  assert(values.front() == wire_offset - bit_size);
+  assert(wire_offset == tree_algo.n_wires_);
+  assert(tree_algo.gates_.size() == (num_inputs - 1) * sub_algo.n_gates_);
+  assert(tree_algo.gates_.size() == tree_algo.n_gates_);
+
+  for (std::size_t i = 0; i < tree_algo.n_gates_; ++i) {
+    [[maybe_unused]] const auto& op = tree_algo.gates_.at(i);
+    assert(op.parent_a_ < tree_algo.n_wires_ - bit_size);
+    assert(!op.parent_b_.has_value() || *op.parent_b_ < tree_algo.n_wires_ - bit_size);
+    assert(op.output_wire_ < tree_algo.n_wires_);
+  }
+  {
+    auto gates = tree_algo.gates_;
+    std::sort(std::begin(gates), std::end(gates),
+              [](const auto& x, const auto& y) { return x.output_wire_ < y.output_wire_; });
+    for (std::size_t i = 0; i < gates.size() - 1; ++i) {
+      assert(gates.at(i).output_wire_ != gates.at(i + 1).output_wire_);
+    }
+  }
+  if (num_inputs == 2) {
+    assert(sub_algo.n_gates_ == tree_algo.n_gates_);
+    assert(sub_algo.n_wires_ == tree_algo.n_wires_);
+    for (std::size_t i = 0; i < tree_algo.n_gates_; ++i) {
+      const auto& sg = sub_algo.gates_.at(i);
+      const auto& tg = tree_algo.gates_.at(i);
+      assert(sg.type_ == tg.type_);
+      assert(sg.parent_a_ == tg.parent_a_);
+      assert(sg.parent_b_ == tg.parent_b_);
+      assert(sg.output_wire_ == tg.output_wire_);
+    }
+  }
+
+  algo_cache_[name] = std::move(tree_algo);
+  return algo_cache_[name];
+}
+
+const ENCRYPTO::AlgorithmDescription& CircuitLoader::load_maxpool_circuit(std::size_t bit_size,
+                                                                          std::size_t num_inputs) {
+  const auto name = fmt::format("__circuit_loader_builtin__gtmux_{}_bit", bit_size);
+  load_gtmux_circuit(bit_size);
+  return load_tree_circuit(name, bit_size, num_inputs);
+}
+
 }  // namespace MOTION
--- src/motioncore/algorithm/circuit_loader.h
+++ src/motioncore/algorithm/circuit_loader.h
@@ -44,6 +44,11 @@ class CircuitLoader {
   const ENCRYPTO::AlgorithmDescription& load_relu_circuit(std::size_t bit_size);
   const ENCRYPTO::AlgorithmDescription& load_gt_circuit(std::size_t bit_size);
   const ENCRYPTO::AlgorithmDescription& load_gtmux_circuit(std::size_t bit_size);
+  const ENCRYPTO::AlgorithmDescription& load_tree_circuit(const std::string& algo_name,
+                                                          std::size_t bit_size,
+                                                          std::size_t num_inputs);
+  const ENCRYPTO::AlgorithmDescription& load_maxpool_circuit(std::size_t bit_size,
+                                                             std::size_t num_inputs);
 
  private:
   std::vector<std::filesystem::path> circuit_search_path_;
